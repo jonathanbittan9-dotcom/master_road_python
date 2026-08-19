e04d79d7da1c57cbe549c824729dfb9c46097d64
jonathanbittan9-dotcom <jonathanbittan9-dotcom@users.noreply.github.com>
2026-08-19 15:50:50 +0300

    g

--- challenges.py
+++ challenges.py
@@ -8,7 +8,8 @@ class messages(ABC):
         ...
 
 class slack(messages):
-    def __init__(self, quota: int = 100) -> None:
+    def __init__(self, name , quota: int = 100) -> None:
         self.quota = quota
+        self.name = name
     def notifications():
-        return  
+        return f""
