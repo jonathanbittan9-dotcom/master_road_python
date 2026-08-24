dced2a8c74c837e4c8ae4866b51660a5aec5981e
jonathanbittan9-dotcom <jonathanbittan9-dotcom@users.noreply.github.com>
2026-08-24 20:51:49 +0300

    get method

--- practicing/advanced_oop.py
+++ practicing/advanced_oop.py
@@ -3,12 +3,15 @@ from typing import Protocol
 import functools
 from dataclasses import dataclass
 
-
 class user():
-    def __get_name__(self, name , instance):
+    def __get_name__(self, name , instance) -> None:
         self.private_name = "_" + name
 
+    def __get__(self , instance , name) -> str:
+        if instance is None:
+            return getattr()
 
+    def __set__()
 class user_setup():
     Email = user()
     discord = user()
