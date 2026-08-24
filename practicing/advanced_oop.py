4cf96dac47d6e8afad7172b8be9c94892217b9e1
jonathanbittan9-dotcom <jonathanbittan9-dotcom@users.noreply.github.com>
2026-08-24 21:30:58 +0300

    fix by claude, need to undertand it

--- practicing/advanced_oop.py
+++ practicing/advanced_oop.py
@@ -4,14 +4,19 @@ import functools
 from dataclasses import dataclass
 
 class user():
-    def __get_name__(self, name , instance) -> None:
+    def __set_name__(self, owner, name) -> None:
         self.private_name = "_" + name
 
-    def __get__(self , instance , name) -> str:
+    def __get__(self, instance, owner) -> str:
         if instance is None:
-            return getattr()
+            return self
+        return getattr(instance, self.private_name)
 
-    def __set__()
+    def __set__(self, instance, value) -> None:
+        if not isinstance(value , str):
+            raise ValueError(f"value is not a str.❌")
+        setattr(instance, self.private_name, value)
+
 class user_setup():
     Email = user()
     discord = user()
